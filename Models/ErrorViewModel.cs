10ef3af7b87d6cb434e0f08be627845c88ddbc2e
usama-mirza <usamamirzaufm@gmail.com>
2022-10-18 17:23:46 +0500

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Genesis.Services.Identity.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
